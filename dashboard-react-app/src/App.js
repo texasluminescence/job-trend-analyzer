2d2a918ee19a579b54f84e508d2a2f878ecb3ef7
AbbyKremer <124317115+AbbyKremer@users.noreply.github.com>
2024-10-30 18:11:11 -0500

    Added fast api backend

--- dashboard-react-app/src/App.js
+++ dashboard-react-app/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Call the FastAPI backend
+    fetch('http://127.0.0.1:8000/test')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="App">
-      <h1>Job Analyzer</h1>
+        <h1>Job Analyzer</h1>
+        <p>Message from API: {message}</p>
     </div>
   );
 }
